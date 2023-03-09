b02626e03c456cbfd6e95726804f5527a781e920
elijah <cuiyeqing960904@gmail.com>
2023-03-09 09:17:14 -0500

    connecting to db

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+	_, err := gorm.Open(
+		postgres.Open("postgres://postgres:root@localhost:5432/service_layer_fiber"),
+		&gorm.Config{},
+	)
+
+	if err != nil {
+		panic("can not connect to postgres")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
