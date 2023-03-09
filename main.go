7badf3e2dda418fed4afe571327c795b49a52be2
elijah <cuiyeqing960904@gmail.com>
2023-03-09 15:03:23 -0500

    building routes

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
+	"github/co-codin/service-layer-fiber/src/database"
+
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := gorm.Open(
-		postgres.Open("postgres://postgres:root@localhost:5432/service_layer_fiber"),
-		&gorm.Config{},
-	)
-
-	if err != nil {
-		panic("can not connect to postgres")
-	}
+	database.Connect()
 
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("Hello, da 👋!")
 	})
 
 	app.Listen(":3000")
--- src/database/db.go
+++ src/database/db.go
@@ -1,1 +1,27 @@
 package database
+
+import (
+	"github/co-codin/service-layer-fiber/src/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+
+	DB, err = gorm.Open(
+		postgres.Open("postgres://postgres:root@localhost:5432/service_layer_fiber"),
+		&gorm.Config{},
+	)
+
+	if err != nil {
+		panic("can not connect to postgres")
+	}
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(models.User{})
+}
